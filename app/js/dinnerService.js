d2a2ea7ab978300a6f6f3dcc894c3366c67f2703
HeyLuka <heyhippo2013@gmail.com>
2016-03-06 17:23:58 +0100

    0306 first commit

--- app/js/dinnerService.js
+++ app/js/dinnerService.js
@@ -16,6 +16,9 @@ dinnerPlannerApp.factory('Dinner',function ($resource) {
     return numberOfGuest;
   }
 
+  this.DishSearch = $resource('http://api.bigoven.com/recipes', {pg:1, rpp:25, api_key:'H9n1zb6es492fj87OxDtZM9s5sb29rW3'});
+  this.Dish = $resource('http://api.bigoven.com/recipe/:id', {api_key:'H9n1zb6es492fj87OxDtZM9s5sb29rW3'});
+
 
   // TODO in Lab 5: Add your model code from previous labs
   // feel free to remove above example code
