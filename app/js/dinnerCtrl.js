6ad41fd3f1f4cfa47d7b32415499b41d2d06d77d
HeyLuka <heyhippo2013@gmail.com>
2016-03-06 23:10:43 +0100

    0306 search all recipes done.

--- app/js/dinnerCtrl.js
+++ app/js/dinnerCtrl.js
@@ -15,4 +15,8 @@ dinnerPlannerApp.controller('DinnerCtrl', function ($scope,Dinner) {
   // TODO in Lab 5: Implement the methods to get the dinner menu
   // add dish to menu and get total menu price
 
+  // $scope.dishes = Dinner.DishSearch.get({title_kw:query});
+
+  
+
 });
