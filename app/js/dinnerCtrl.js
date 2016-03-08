af77515bfb54293a96c146fca7c373c22e2b0b75
lyl <869494563@qq.com>
2016-03-08 22:02:02 +0100

    implement sidebar

--- app/js/dinnerCtrl.js
+++ app/js/dinnerCtrl.js
@@ -16,7 +16,54 @@ dinnerPlannerApp.controller('DinnerCtrl', function ($scope,Dinner) {
   // add dish to menu and get total menu price
 
   // $scope.dishes = Dinner.DishSearch.get({title_kw:query});
+  var menu = Dinner.getMenu();
+
+  $scope.addDishToMenu = function(id){
+    Dinner.addDishToMenu(id);
+  }
+  
+  $scope.removeDishFromMenu = function(id){
+    Dinner.removeDishFromMenu(id);
+  }
+
+  $scope.totalMenuPrice = 0;
+  $scope.menuTitlePriceList = [];
+  for(var key in menu){
+    (function(key){
+      Dinner.Dish.get({id: menu[key]}, function(data){
+        var title = data.Title;
+        var ingredients = data.Ingredients;
+        var dishPrice = 0;
+
+        for(var ing_key in ingredients){
+          dishPrice += ingredients[ing_key].Quantity;
+        }
+        $scope.test = 1;
+
+        $scope.menuTitlePriceList[key] = ({"title":title, "dishPrice":fixNumber(dishPrice)})
+        //alert($scope.menuTitlePriceList[key]);
+        $scope.totalMenuPrice = fixNumber($scope.totalMenuPrice + dishPrice);
+      })
+    })(key);
+  }
+
+  $scope.pending = 0;
+
+  var fixNumber = function(number){
+    var numberString = number.toString();
+    var decimalIndex=numberString.indexOf('.');
+    if((decimalIndex == '-1') || (numberString.substring(decimalIndex+1,numberString.length).length < 5)){
+      return number;
+    }else{
+      return parseFloat(number.toFixed(2));
+    }
+  }
+
+  //alert(1);
+  //alert(2);
 
   
+
+
 
 });
--- app/js/dinnerService.js
+++ app/js/dinnerService.js
@@ -6,6 +6,29 @@
 dinnerPlannerApp.factory('Dinner',function ($cookieStore, $resource) {
   
   var numberOfGuest = 2;
+  var menu = ["167511", "164277"];
+
+  this.getMenu = function(){
+    return menu;
+  }
+
+  this.addDishToMenu = function(id){
+    for(var key in menu){
+      if(menu[key] == id){
+        return;
+      }
+    }
+    menu.push(id);
+  }
+
+  this.removeDishFromMenu = function(id){
+    for(var key in menu){
+      if(menu[key] == id){
+        menu.splice(key,1);
+        break;
+      }
+    }
+  }
 
 
   this.setNumberOfGuests = function(num) {
@@ -18,9 +41,24 @@ dinnerPlannerApp.factory('Dinner',function ($cookieStore, $resource) {
     // return numberOfGuest;
     return $cookieStore.get("numberOfGuest");
   }
+  //10000 per hour key:
+  var api_key = "sV1fPGQKrO0b6oUYb6w9kLI8BORLiWox";
+  //500 per hour key:
+  //var api_key = "0OV23011kU7B3VVVgxTTTIfdNXeTI3us";
+  //var api_key = "66J8l00npnHHZcCNLRhxkfW1OHxbojy4";
+  //var api_key = "XKEdN82lQn8x6Y5jm3K1ZX8L895WUoXN";
+  //var api_key = "3stL5NVP4s6ZkmK5gt4dci8a4zOQRpD4";
+  //var api_key = "8vtk7KykflO5IzB96kb0mpot0sU40096";
+  //var api_key = "1hg3g4Dkwr6pSt22n00EfS01rz568IR6";
+  //var api_key = "r02x0R09O76JMCMc4nuM0PJXawUHpBUL";
+  //var api_key = "H9n1zb6es492fj87OxDtZM9s5sb29rW3";
+  //var api_key = "18f3cT02U9f6yRl3OKDpP8NA537kxYKu";
 
-  this.DishSearch = $resource('http://api.bigoven.com/recipes', {pg:1, rpp:12, api_key:'1hg3g4Dkwr6pSt22n00EfS01rz568IR6'});
-  this.Dish = $resource('http://api.bigoven.com/recipe/:id', {api_key:'1hg3g4Dkwr6pSt22n00EfS01rz568IR6', id:164511});
+
+  this.DishSearch = $resource('http://api.bigoven.com/recipes', {pg:1, rpp:12, api_key: api_key});
+  this.Dish = $resource('http://api.bigoven.com/recipe/:id', {api_key: api_key});
+
+
 
 
   // TODO in Lab 5: Add your model code from previous labs
